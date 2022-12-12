f00c284651f3161f3b9928fd910a3d249d6c3f46
Alisson <alissonhmb@gmail.com>
2022-12-12 17:45:28 -0300

    association annotation user

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,7 +1,7 @@
 class Annotation < ApplicationRecord
   scope :search_title_or_annotation, -> (title) { where("unaccent(title)  ILIKE unaccent(?) OR unaccent(annotation) ILIKE unaccent(?)", "%#{title}%", "%#{title}")}
 
-  has_one :user
+  belongs_to :user
   enum priority:  [:baixa, :media, :alta]
   validates :title, :annotation, presence: true
 end
